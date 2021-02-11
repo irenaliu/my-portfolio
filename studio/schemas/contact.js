57d4e3241c2fa907c687205c04451541abb83f40
irenaliu <irena@ualberta.ca>
2021-02-10 18:26:45 -0700

    Add Contact form schema

---
+++ studio/schemas/contact.js
@@ -0,0 +1,27 @@
+export default {
+  name: 'contact',
+  title: 'Contact submissions',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'phone',
+      title: 'Phone',
+      type: 'string',
+    },
+    {
+      name: 'message',
+      title: 'Message',
+      type: 'text',
+    },
+  ],
+};
