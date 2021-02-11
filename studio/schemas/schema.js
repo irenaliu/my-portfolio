81ad7ee981b77870ee203c1aa47df854e8cd183e
irenaliu <irena@ualberta.ca>
2021-02-10 18:27:29 -0700

    Modified schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -10,6 +10,7 @@ import category from './category';
 import post from './post';
 import project from './project';
 import author from './author';
+import contact from './contact';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -27,5 +28,6 @@ export default createSchema({
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
+    contact,
   ]),
 });
