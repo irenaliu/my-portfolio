a30ff1ffaaa0adf4e9989f2b90480b1be05d8f17
irenaliu <irena@ualberta.ca>
2021-03-10 19:39:10 -0700

    Removed margin from nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export default function Navbar({ fixed }) {
 
   return (
     <header className='bg-primary opacity-80 absolute inset-x-0 top-0'>
-      <nav className='relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg mb-3'>
+      <nav className='relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg'>
         <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
           <div className='w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start'>
             <NavLink
