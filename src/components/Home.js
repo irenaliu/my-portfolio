d0a073d513d10930f80bdcd11777a62824fdfdb1
irenaliu <irena@ualberta.ca>
2021-03-10 18:21:15 -0700

    Update welcome message styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,8 @@ export default function Home() {
         alt='Trees and river'
         className='absolute object-cover w-full h-full'
       />
-      <section className='relative flex justify-center min-h-screen pt-12 lg:pt-72 px-8'>
-        <h1 className='text-6xl text-green-100 font-bold sans-serif leading-none lg:leading-snug home-name'>
+      <section className='relative flex justify-center min-h-screen pt-52 md:pt-72 px-8'>
+        <h1 className='text-7xl lg:text-9xl text-green-100 font-bold sans-serif leading-none lg:leading-snug'>
           Hello! I'm Irena.
         </h1>
       </section>
